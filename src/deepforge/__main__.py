51cabcab6b1debfa03f309f16ee2420f4b9084b4
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2023-10-11 17:11:51 +0200

    Fixing example

--- src/deepforge/__main__.py
+++ src/deepforge/__main__.py
@@ -5,8 +5,8 @@ from deepforge import RNN, CNN
 def main():
     """Instantiate two DNNs"""
 
-    _RNN = RNN(layers=3,name="Recurrent Neural Network")
-    _CNN = CNN(layers=4,name="Convolutional Neural Network")
+    _RNN = RNN(name="Recurrent Neural Network")
+    _CNN = CNN(name="Convolutional Neural Network")
     print(_RNN.getName())
     print(_CNN.getName())
 
