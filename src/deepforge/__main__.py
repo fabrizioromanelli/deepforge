3c6e1e29d442e26326143f399f0be340d488e70e
Fabrizio Romanelli <fabrizio.romanelli@gmail.com>
2023-11-24 09:38:20 +0100

    Refactoring

--- src/deepforge/__main__.py
+++ src/deepforge/__main__.py
@@ -1,14 +1,20 @@
 # __main__.py
 
-from deepforge import RNN, CNN
+from deepforge import RNN, CNN, CRNN, DAE, multivariateDNN
 
 def main():
-    """Instantiate two DNNs"""
+    """Instantiate several DNNs"""
 
-    _RNN = RNN(name="Recurrent Neural Network")
-    _CNN = CNN(name="Convolutional Neural Network")
+    _RNN  = RNN(name="Recurrent Neural Network")
+    _CNN  = CNN(name="Convolutional Neural Network")
+    _CRNN = CRNN(name="Convolutional Recurrent Neural Network")
+    _DAE  = DAE(name="Denoising AutoEncoder")
+    _mDNN = multivariateDNN(name="Multivariate Deep Neural Network")
     print(_RNN.getName())
     print(_CNN.getName())
+    print(_CRNN.getName())
+    print(_DAE.getName())
+    print(_mDNN.getName())
 
 if __name__ == "__main__":
     main()
